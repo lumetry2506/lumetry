3c851062d791c162e56b38eb885550a001431d9f
lumetry2506 <lumetry2506@gmail.com>
2025-07-09 17:53:09 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBufbOgPXVxsZoNicVUZg2aOEaeLpKyVA8",
+  authDomain: "lumetry-cc7cc.firebaseapp.com",
+  projectId: "lumetry-cc7cc",
+  storageBucket: "lumetry-cc7cc.firebasestorage.app",
+  messagingSenderId: "33571840437",
+  appId: "1:33571840437:web:25680c845b4857493079fc",
+  measurementId: "G-VX6Z2L9XW6"
+};
